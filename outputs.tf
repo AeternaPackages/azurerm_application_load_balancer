219c2b5f322c7ab5f21b9ef5ab19db45990c224c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:37:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 # --- azurerm_application_load_balancer ---
+output "application_load_balancers_id" {
+  description = "Map of id values across all application_load_balancers, keyed the same as var.application_load_balancers"
+  value       = module.application_load_balancers.application_load_balancers_id
+}
+
 output "application_load_balancers_location" {
   description = "Map of location values across all application_load_balancers, keyed the same as var.application_load_balancers"
   value       = module.application_load_balancers.application_load_balancers_location
@@ -25,6 +30,11 @@ output "application_load_balancers_tags" {
 }
 
 # --- azurerm_application_load_balancer_frontend ---
+output "application_load_balancer_frontends_id" {
+  description = "Map of id values across all application_load_balancer_frontends, keyed the same as var.application_load_balancer_frontends"
+  value       = module.application_load_balancer_frontends.application_load_balancer_frontends_id
+}
+
 output "application_load_balancer_frontends_application_load_balancer_id" {
   description = "Map of application_load_balancer_id values across all application_load_balancer_frontends, keyed the same as var.application_load_balancer_frontends"
   value       = module.application_load_balancer_frontends.application_load_balancer_frontends_application_load_balancer_id
@@ -46,6 +56,11 @@ output "application_load_balancer_frontends_tags" {
 }
 
 # --- azurerm_application_load_balancer_security_policy ---
+output "application_load_balancer_security_policies_id" {
+  description = "Map of id values across all application_load_balancer_security_policies, keyed the same as var.application_load_balancer_security_policies"
+  value       = module.application_load_balancer_security_policies.application_load_balancer_security_policies_id
+}
+
 output "application_load_balancer_security_policies_application_load_balancer_id" {
   description = "Map of application_load_balancer_id values across all application_load_balancer_security_policies, keyed the same as var.application_load_balancer_security_policies"
   value       = module.application_load_balancer_security_policies.application_load_balancer_security_policies_application_load_balancer_id
@@ -72,6 +87,11 @@ output "application_load_balancer_security_policies_web_application_firewall_pol
 }
 
 # --- azurerm_application_load_balancer_subnet_association ---
+output "application_load_balancer_subnet_associations_id" {
+  description = "Map of id values across all application_load_balancer_subnet_associations, keyed the same as var.application_load_balancer_subnet_associations"
+  value       = module.application_load_balancer_subnet_associations.application_load_balancer_subnet_associations_id
+}
+
 output "application_load_balancer_subnet_associations_application_load_balancer_id" {
   description = "Map of application_load_balancer_id values across all application_load_balancer_subnet_associations, keyed the same as var.application_load_balancer_subnet_associations"
   value       = module.application_load_balancer_subnet_associations.application_load_balancer_subnet_associations_application_load_balancer_id
